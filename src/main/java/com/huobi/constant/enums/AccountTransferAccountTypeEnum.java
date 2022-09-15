b4d3beaff0a4017015f6378f9bac761a05fd60df
RomanDiachuk <rwdiachuk@gmail.com>
2022-09-15 09:55:58 +0200

    + symbols info req

--- src/main/java/com/huobi/constant/enums/AccountTransferAccountTypeEnum.java
+++ src/main/java/com/huobi/constant/enums/AccountTransferAccountTypeEnum.java
@@ -3,14 +3,18 @@ package com.huobi.constant.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 @AllArgsConstructor
 public enum  AccountTransferAccountTypeEnum {
-
   SPOT("spot"),
-  MARGIN("margin"),
-
-  ;
+  MARGIN("margin");
   private String accountType;
 
+  public static AccountTransferAccountTypeEnum fromName(String name){
+    return Arrays.stream(values())
+            .filter(wallet -> wallet.getAccountType().equalsIgnoreCase(name))
+            .findFirst().orElseThrow(() -> new IllegalArgumentException("Unknown wallet type: " + name));
+  }
 }
