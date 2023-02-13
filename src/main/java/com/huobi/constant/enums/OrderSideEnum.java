d7ad3b13f70e67e503273544bbf91c822deb2d62
RomanDiachuk <rwdiachuk@gmail.com>
2023-02-13 15:39:38 +0100

    parse OrderSideEnum

--- src/main/java/com/huobi/constant/enums/OrderSideEnum.java
+++ src/main/java/com/huobi/constant/enums/OrderSideEnum.java
@@ -13,4 +13,13 @@ public enum OrderSideEnum {
   SELL("sell");
 
   private final String code;
+
+  public static OrderSideEnum find(String code) {
+    for (OrderSideEnum sideEnum : OrderSideEnum.values()) {
+      if (sideEnum.getCode().equals(code)) {
+        return sideEnum;
+      }
+    }
+    throw new IllegalArgumentException("Not a valid order side: " + code);
+  }
 }
